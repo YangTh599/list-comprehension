76bbc9ef802cb1c5220413a4c897167d944bf65d
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-11-03 08:27:41 -0500

    Update list_comprehension_demo1.py

--- list_comprehension_demo1.py
+++ list_comprehension_demo1.py
@@ -1,2 +1,14 @@
+# Use list comprehension to generate a new list containing the squares of the numbers 1 - 10
 squares = [x**2 for x in range(1, 11)]
 print(squares)
+
+
+
+# Make a new list of names written in title case
+last_names = ["johnson", "smith", "williams", "brown", "jones"]
+
+title_case_names = [name.title() for name in last_names]
+
+print(title_case_names)
+
+
